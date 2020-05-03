a62b4a5af8bfe8aab3143abcf0a1d8a4ef369ffa
Sumukh Subramanya H K <51911251+sumukhhk@users.noreply.github.com>
2020-05-03 23:16:19 +0530

    Update FOL_TO_CNF.py

--- FOL_TO_CNF.py
+++ FOL_TO_CNF.py
@@ -1,73 +1,62 @@
-print("^ for and, + for or, ! for not, > for implies, = for biconditional")
-exp=str(input("Enter the expression"))
-l=list(exp)
-def search(l):
-    if '=' in l:
-        return l.index('=')
-    elif '>' in l:
-        return l.index('>')
-    elif '!' in l:
-        return l.index('!')
-    elif '^' in l:
-        return l.index('^')
-    elif '+' in l:
-        return l.index('+')
+prep=input("Enter the preposition logic:       ")
+di=prep.find('<=>')
+if di == -1:
+    pass
+else:
+    print(di)
+    prep=prep[0:di]+"=>"+prep[di+3:len(prep)]+" ^ "+prep[di+3:]+"=>"+prep[:di]
+    print("\n1.Removing double implies:      "+prep)
+i=prep.find('=>')
+while(i!=-1):
+    if prep[i-1]!=")":
+        prep=prep[0:i-1]+"!"+prep[i-1]+" v "+prep[i+2:]
     else:
-        return None
-def bi_rule(left,right):
-    l=[]
-    r=[]
-    for i in left:
-        l.append(i)
-    l.append('>')
-    for j in right:
-        l.append(j)
-    for i in right:
-        r.append(i)
-    r.append('>')
-    for j in left:
-        r.append(j)
-    return l,r
-def imp_rule(left,right):
-    res=[]
-    res.append('!(')
-    for i in left:
-        res.append(i)
-    res.append(')+(')
-    for j in right:
-        res.append(j)
-    res.append(')')
-    return res
-def distri(l):
-    if l[1]=='^' and l[4]=='+':
-        exp="("+l[0]+"^"+l[3]+")+("+l[0]+"^"+l[5]+")"
-    elif l[1]=='+' and l[4]=='^':
-        exp="("+l[0]+"+"+l[3]+")^("+l[0]+"+"+l[5]+")"
+        '''ib=prep.find('(')'''
+        j=i
+        while prep[j]!='(':
+            j-=1
+        prep=prep[0:j]+"!"+prep[j:i]+" v "+prep[i+2:]
+    i=prep.find('=>')
+print("\n2.Removing single implies:      "+prep)
+
+
+n=prep.find('!(')
+while(n!=-1):
+    if prep[n+1]=='(':
+        l=len(prep)
+        prepn=prep
+        prep=prepn[0:n+1]
+        i=n+2
+        while(prepn[i]!='^' and prepn[i]!='v'):
+            if(prepn[i]!='('):
+                prep+=prepn[i]
+            i+=1
+        if prepn[i]=='^':
+            prep+=' v '
+        elif prepn[i]=='v':
+            prep+=' ^ '
+        prep+='!'+prepn[i+1:prepn.find(')',i)]
+        prep+=prepn[prepn.find(')',i)+1:]
+    n=prep.find('!(')
+print("\n3.Demorgan's Law:               "+prep)
+
+prep = prep.replace("! !", "")
+print("\n4.Removing double negation:     "+prep)
+
+prepn=prep
+prep=""
+for i in prepn:
+    if i!='(' and i!=')':
+        prep+=i
+print("\n5.Removing unwanted brackets:   "+prep)
+
+prepn=prep
+prep="("
+for i in prepn:
+    if i=='^':
+        prep+=') ^ ('
     else:
-        exp=l
-    return exp
-pos=search(l)
-left=l[:pos]
-right=l[pos+1:]
-if l[pos]=='=':
-    print("Applying biconditional elimination")
-    leftimp,rightimp=bi_rule(left,right)
-    lInd=search(leftimp)
-    rInd=search(rightimp)
-    leftA=leftimp[:lInd]
-    leftB=leftimp[lInd+1:]
-    rightA=rightimp[:rInd]
-    rightB=rightimp[rInd+1:]
-    print("Applying implication elimination")
-    leftExp=imp_rule(leftA,leftB)
-    rightExp=imp_rule(rightA,rightB)
-    fin="("+"".join(leftExp)+")^("+"".join(rightExp)+")"
-    print(fin)
-elif l[pos]=='>':
-    print("Applying implication elimination")
-    fin="".join(imp_rule(left,right))
-    print(fin)
-elif l[pos]=="^" or l[pos]=="+":
-    print("Applying distributive rule")
-    fin="".join(distri(l))
-    print(fin)
+        prep+=i
+prep+=')'
+print("\n6.Grouping into CNF form:       "+prep)
+
